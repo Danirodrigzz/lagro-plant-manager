47d0e51d85dae81b371facb74bf3cfc2cf15ffbe
Daniela Rodriguez <natachaa0424@gmail.com>
2026-03-01 21:59:53 -0400

    chore(frontend): add test folder and remaining project infrastructure

--- frontend/lib/features/products/presentation/providers/products_providers.dart
+++ frontend/lib/features/products/presentation/providers/products_providers.dart
@@ -1,18 +1,26 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/network/dio_client.dart';
+import '../../data/datasources/products_local_data_source.dart';
 import '../../data/datasources/products_remote_data_source.dart';
 import '../../data/repositories/product_repository_impl.dart';
 import '../../domain/repositories/i_product_repository.dart';
 import '../../domain/usecases/get_products.dart';
 
-// 1. Data Source
+// 1. Data Sources
 final productsRemoteDataSourceProvider = Provider<ProductsRemoteDataSource>((ref) {
   return ProductsRemoteDataSource(DioClient.instance);
 });
 
+final productsLocalDataSourceProvider = Provider<ProductsLocalDataSource>((ref) {
+  return ProductsLocalDataSource();
+});
+
 // 2. Repository
 final productRepositoryProvider = Provider<IProductRepository>((ref) {
-  return ProductRepositoryImpl(ref.watch(productsRemoteDataSourceProvider));
+  return ProductRepositoryImpl(
+    ref.watch(productsRemoteDataSourceProvider),
+    ref.watch(productsLocalDataSourceProvider),
+  );
 });
 
 // 3. Use Case
